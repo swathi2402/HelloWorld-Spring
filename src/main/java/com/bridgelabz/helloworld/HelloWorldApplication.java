2ce1dbd18cdac0e0e9832939e6619248adefde13
Swathi <swathi.h@perfios.com>
2021-10-28 21:20:56 +0530

    ADDED: Initializing project structure

---
+++ src/main/java/com/bridgelabz/helloworld/HelloWorldApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello From Bridgelabz");
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
