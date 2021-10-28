6f019128cf67c56d61919ec52af99aa5dbcdaa6a
Swathi <swathi.h@perfios.com>
2021-10-28 22:01:55 +0530

    ADDED: Used MVC controller to invoke HTML

--- src/main/java/com/bridgelabz/controller/HelloWebController.java
+++ src/main/java/com/bridgelabz/controller/HelloWebController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,4 +12,9 @@ public class HelloWebController {
 		return "hello";
 	}
 
+	@GetMapping("/web/message")
+	public String message(Model model) {
+		model.addAttribute("message", "This is a custom message");
+		return "message";
+	}
 }
