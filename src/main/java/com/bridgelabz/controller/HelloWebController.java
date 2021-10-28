08c644b86a64489593974eb5b0669a3226157c8a
Swathi <swathi.h@perfios.com>
2021-10-28 21:58:18 +0530

    ADDED: Displaying welcome message in web page using HTML and controller

---
+++ src/main/java/com/bridgelabz/controller/HelloWebController.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWebController {
+
+	@GetMapping("/web")
+	public String hello() {
+		return "hello";
+	}
+
+}
--- src/main/java/com/bridgelabz/helloworld/HelloWorldApplication.java
+++ src/main/java/com/bridgelabz/helloworld/HelloWorldApplication.java
@@ -2,8 +2,10 @@ package com.bridgelabz.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.bridgelabz.controller"})
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
